bfdcb3ad9187266e93856095677c354fb505dd28
Harsh Pandit <93505687+HarshPanditXD@users.noreply.github.com>
2021-12-18 09:38:15 +0530

    Update music.py

--- program/music.py
+++ program/music.py
@@ -47,8 +47,8 @@ async def play(c: Client, m: Message):
     keyboard = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton(text="• Mᴇɴᴜ", callback_data="cbmenu"),
-                InlineKeyboardButton(text="• Cʟᴏsᴇ", callback_data="cls"),
+                InlineKeyboardButton(text="•✌️ Mᴇɴᴜ ✌️", callback_data="cbmenu"),
+                InlineKeyboardButton(text="•✨ Cʟᴏsᴇ 🗑️", callback_data="cls"),
             ]
         ]
     )
